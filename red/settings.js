5ef6df460c59e95e647301848b76d2c0983e5ddd
Fabio <fabio.michelini@gmail.com>
2020-07-07 20:59:38 +0200

    removed unnecessary 'exportGlobalContextKeys' setting

--- red/settings.js
+++ red/settings.js
@@ -21,7 +21,6 @@ module.exports = {
     flowFilePretty: true,
     credentialSecret: false,
     userDir: flowsDir,
-    exportGlobalContextKeys: true,
     adminAuth: {
         type: 'credentials',
         users: authUsers,
