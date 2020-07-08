7f3e6f09aec7090598b932ccd8e460a291ab6d9e
Fabio <fabio.michelini@gmail.com>
2020-07-08 18:11:20 +0200

    first java commit

---
+++ java/src/main/java/it/mamosk/kata/apisocial/ApisocialApplication.java
@@ -0,0 +1,13 @@
+package it.mamosk.kata.apisocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisocialApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/it/mamosk/kata/apisocial/ApisocialApplicationTests.java
@@ -0,0 +1,13 @@
+package it.mamosk.kata.apisocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
