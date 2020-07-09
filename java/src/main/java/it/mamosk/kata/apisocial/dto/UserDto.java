eb0c7a361a64188671c63e9a023dcdac4a615839
Fabio <fabio.michelini@gmail.com>
2020-07-09 19:31:02 +0200

    user dto contains only a list of usernames, not a list of full users

--- java/src/main/java/it/mamosk/kata/apisocial/dto/UserDto.java
+++ java/src/main/java/it/mamosk/kata/apisocial/dto/UserDto.java
@@ -2,16 +2,13 @@ package it.mamosk.kata.apisocial.dto;
 
 import java.util.Set;
 
-import javax.validation.constraints.NotEmpty;
-
 import lombok.Data;
-import lombok.NonNull;
 
 @Data
 public class UserDto {
 
 	private String name;
 
-	private Set<UserDto> follows;
+	private Set<String> follows;
 
 }
