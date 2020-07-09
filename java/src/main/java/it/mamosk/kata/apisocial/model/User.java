139ad907d67e494ad55016a3979f6cd7f62e8299
Fabio <fabio.michelini@gmail.com>
2020-07-09 19:33:05 +0200

    many-to-many for followers (everyone can follow everyone, but not a graph)

--- java/src/main/java/it/mamosk/kata/apisocial/model/User.java
+++ java/src/main/java/it/mamosk/kata/apisocial/model/User.java
@@ -1,18 +1,20 @@
 package it.mamosk.kata.apisocial.model;
 
-import static javax.persistence.CascadeType.PERSIST;
-import static javax.persistence.FetchType.EAGER;
+import static javax.persistence.FetchType.LAZY;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -22,18 +24,20 @@ import lombok.RequiredArgsConstructor;
 @Table(name = User.TABLE)
 @RequiredArgsConstructor
 @NoArgsConstructor // required by Hibernate
+@EqualsAndHashCode(exclude = "follows")
 public class User {
 
 	// *******************************************************************
 	// *******************************************************************
 	// *******************************************************************
 
-	// table
+	// tables
 	public static final String TABLE = "users";
+	public static final String JOINTABLE = "followers";
 
 	// columns
-	private static final String NAME = "name";
-	private static final String FOLLOWS = "follows";
+	public static final String NAME = "name";
+	public static final String FOLLOWS = "follows";
 
 	// *******************************************************************
 	// *******************************************************************
@@ -43,10 +47,22 @@ public class User {
 	@Column(name = NAME)
 	private @NonNull String name;
 
-	@Column(name = FOLLOWS)
-	@OneToMany(fetch = EAGER, cascade = PERSIST)
-	@JoinColumn(name = NAME)
-	private Set<User> follows;
+	@ManyToMany(fetch = LAZY)
+	@JoinTable(name = JOINTABLE, //
+			joinColumns = { @JoinColumn(name = NAME) }, //
+			inverseJoinColumns = { @JoinColumn(name = FOLLOWS) } //
+	)
+	private Set<User> follows = new HashSet<>();
+
+	// *******************************************************************
+	// *******************************************************************
+	// *******************************************************************
+
+	@Override
+	// no need to know about followers
+	public String toString() {
+		return getName();
+	}
 
 	// *******************************************************************
 	// *******************************************************************
