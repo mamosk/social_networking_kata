fe84bffff62573466279da0670c1c2fbf16ccb27
Fabio <fabio.michelini@gmail.com>
2020-07-11 18:32:40 +0200

    no csharp

---
+++ csharp/katacli/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using McMaster.Extensions.Hosting.CommandLine;
+using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace katacli
+{
+    class Program
+    {
+        private static async Task Main(string[] args) => await CreateHostBuilder(args).Build().RunCommandLineApplicationAsync<IKataCli>();
+        //var Configuration = new ConfigurationBuilder()
+        //    .
+        //    .SetBasePath(Directory.GetCurrentDirectory())
+        //    .AddJsonFile(AppDomain.CurrentDomain.BaseDirectory + "\\appsettings.json", optional: true, reloadOnChange: true)
+        //    .AddEnvironmentVariables()
+        //    .Build();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(host =>
+                    host.SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddEnvironmentVariables(prefix: "CLI_")
+                )
+                .ConfigureServices((context, services) =>
+                    services.AddSingleton<IKataCli, KataCli>()
+                        .AddHttpClient()
+                )
+            ;
+    }
+}
