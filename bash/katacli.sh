8d77931a368b6aebc4e910c211202cc81a870492
Fabio <fabio.michelini@gmail.com>
2020-07-12 07:21:14 +0200

    comments in cli script

--- bash/katacli.sh
+++ bash/katacli.sh
@@ -10,14 +10,21 @@ set -eo pipefail
 # cli prefix
 KATA="kata>"
 
+#########################################
+### ENTRY POINT IS AT THE END OF FILE ###
+#########################################
+
+### UNRECOGNIZED COMMAND ###
 unrecognized() {
   echo "$KATA unrecognized command: $1" 1>&2
 }
 
+### README COMMAND ###
 readme() {
   curl -s "https://raw.githubusercontent.com/xpeppers/social_networking_kata/master/README.md" | pandoc | lynx -stdin
 }
 
+### HELP COMMAND ###
 help() {
   cat <<-EOF
 
@@ -36,6 +43,12 @@ kata full readme: https://github.com/xpeppers/social_networking_kata
 EOF
 }
 
+### POSTING COMMAND ###
+# cmd:  <user name> -> <message>
+# info: posts message to user timeline
+# args: $1 user name
+#       $2 -> (not used)
+#       $3... message to be posted
 posting() {
   curl -s --location --request POST "http://localhost:11881/posting?user=$1" \
     --header 'Content-Type: text/plain' \
@@ -43,11 +56,20 @@ posting() {
     | jq .
 }
 
+### READING COMMAND ###
+# cmd:  <user name>
+# info: reads messages from user timeline
+# args: $1 user name
 reading() {
   curl -s --location --request GET "http://localhost:11881/reading?user=$1" \
     | jq .
 }
 
+### FOLLOWING COMMAND ###
+# cmd:  <user name> follows <another user>
+# info: subscribes user to another user timeline
+# args: $1 user name (follower)
+#       $2 another user (followed)
 following() {
   curl -s --location --request PUT "http://localhost:11881/following?user=$1" \
     --header 'Content-Type: text/plain' \
@@ -55,35 +77,74 @@ following() {
     | jq .
 }
 
+### WALL COMMAND ###
+# cmd:  <user name> wall
+# info: read messages from user timeline and subscriptions
+# args: $1 user name
 wall() {
   curl -s --location --request GET "http://localhost:11881/wall?user=$1" \
     | jq .
 }
 
-apicall () {
+### MAIN COMMAND STARTING WITH USER NAME ###
+username () {
   # return if empty line
   [ -z $1 ] && return 0
+  # check command after user name
+  # each command calls a specific function
   case $2 in
-    "->") posting "$@";;
-    "") reading $1;;
-    "follows") following $1 $3;;
-    "wall") wall $1;;
-    *) unrecognized $2;;
+    "->")
+      posting "$@"
+      ;;
+    "")
+      reading $1
+      ;;
+    "follows")
+      following $1 $3
+      ;;
+    "wall")
+      wall $1
+      ;;
+    *)
+      unrecognized $2
+      ;;
   esac
 }
 
+### MAIN CLI FUNCTION ###
 kata () {
+  # loop until 'exit' command
   while read -r -e -p "$KATA "
   do
+    # keep command in history to be reused
     history -s "$REPLY"
+    # check command
     case "$REPLY" in
-      "exit") exit 0;;
-      "help") help 0;;
-      *) apicall $REPLY;;
+      # exit command
+      "exit")
+        exit 0
+        ;;
+      # help command
+      "help")
+        help
+        ;;
+      # command starting with user name
+      *)
+        username $REPLY
+        ;;
     esac
   done
 }
 
+################################
+### ENTRY POINT IS DOWN HERE ###
+################################
+
+# move to script directory
 pushd "$(dirname "$0")" > /dev/null
-    kata "$@"
+
+# execute main cli function
+kata "$@"
+
+# restore original directory
 popd > /dev/null
