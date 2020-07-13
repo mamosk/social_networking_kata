2bc659f3bae5cda716a9d815224d3ecfd6eab8eb
Fabio <fabio.michelini@gmail.com>
2020-07-13 14:39:34 +0200

    neat bash script for cli

--- frontend/bash/katacli.sh
+++ frontend/bash/katacli.sh
@@ -1,21 +1,85 @@
 #!/usr/bin/env bash
 set -e -o pipefail -o noglob
 
-# requirements:
-# - curl
-# - jq
-# - pandoc
-# - lynx
-
-#########################################
-### ENTRY POINT IS AT THE END OF FILE ###
-#########################################
-
 # cli prefix
 PREFIX="> "
 
-# words for human-readable <time> ago
-TIMEWORDS=(second minute hour)
+##############
+### ERRORS ###
+##############
+
+### WORD IN USER COMMAND NOT RECOGNIZED ###
+unrecognized() {
+  echo "${PREFIX}unrecognized: $1" 1>&2
+}
+
+### DEPENDENCY NOT INSTALLED ###
+unknown() {
+  echo "${PREFIX}dependency not installed: $1" 1>&2
+  echo "${PREFIX}maybe you can run: apt-get install $1" 1>&2
+}
+
+####################
+### REQUIREMENTS ###
+####################
+
+# dependencies
+DEPENDENCIES=(curl jq pandoc lynx)
+
+### DEPENDENCIES CHECK ###
+depcheck(){
+  for dependency in "${DEPENDENCIES[@]}"
+  do
+    if ! [ -x "$(command -v "$dependency")" ]
+    then
+      unknown "$dependency"
+      exit 1
+    fi
+  done
+}
+
+### ENVIRONMET VARIABLES CHECK ###
+varcheck() {
+  # MODE is mandatory
+  if [ -z "$MODE" ]
+  then
+    echo "${PREFIX}environment variable not set: MODE" >&2
+    echo "${PREFIX}maybe you can run: export MODE=mono" >&2
+    exit 1
+  fi
+  case "$MODE" in
+    "full")
+      # API_BASE_URL is mandatory in 'full' mode
+      if [ -z "$API_BASE_URL" ]
+      then
+        echo "${PREFIX}environment variable not set: API_BASE_URL" >&2
+        echo "${PREFIX}maybe you can run: export API_BASE_URL=http://localhost:11881/" >&2
+        exit 1
+      fi
+    ;;
+    "mono")
+      # MONO_PATH defaults to <script directory>/.mono in 'mono' mode
+      if [ -z "$MONO_PATH" ]
+      then
+        MONO_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P )/.mono"
+      fi
+      ;;
+    *)
+      echo "${PREFIX}mode not available: $MODE" >&2
+      exit 1
+      ;;
+  esac
+}
+
+### ALL-REQUIREMENTS CHECK ###
+reqcheck() {
+  depcheck # check dependencies
+  varcheck # check environment variables
+}
+
+#############
+### UTILS ###
+#############
 
 ### PREPEND PREFIX TO EVERY OUTPUT LINE ###
 # usage:   pipe to a command
@@ -27,40 +91,15 @@ cli() {
   done
 }
 
-### UNRECOGNIZED COMMAND ###
-unrecognized() {
-  echo "${PREFIX}unrecognized command: $1" 1>&2
-}
-
-### README COMMAND ###
-readme() {
-  curl -s "https://raw.githubusercontent.com/xpeppers/social_networking_kata/master/README.md" | pandoc | lynx -stdin
-}
-
-### HELP COMMAND ###
-help() {
-  cat <<-EOF
-
-kata cli commands:
-  <user name> -> <message>           -> post message to user timeline
-  <user name>                        -> read messages from user timeline
-  <user name> follows <another user> -> subscribe user to another user timeline
-  <user name> wall                   -> read messages from user timeline and subscriptions
-
-  exit -> exit the cli
-  help -> read this help
-  kata -> read full readme of kata requirements
-
-kata full readme: https://github.com/xpeppers/social_networking_kata
-
-EOF
-}
-
-### STORE GLOBAL CURRENT TIME IN SECONDS ###
+### CURRENT EPOCH ###
 now() {
   now=$(date -u "+%s")
 }
 
+# words for human-readable <time> ago
+TIMEWORDS=(second minute hour)
+
+### FROM ZULU TIME TO '<n> seconds|minutes|hours ago' ###
 ago() {
   while read -r line
   do
@@ -78,7 +117,7 @@ ago() {
     local diff
     diff=$((now - ago))
     # demo accepts only last 10 seconds
-    [ "$demo" = true ] && [ "$diff" -ge $((commands * 3)) ] && return
+    [ "$demo" = true ] && [ "$diff" -ge $((commands * 3)) ] && continue
     # compute human-readable difference
     local i=0
     while [ $diff -ge 60 ] && [ $i -lt ${#TIMEWORDS[@]} ]
@@ -122,12 +161,12 @@ posting_mono() {
 }
 posting_switch() {
   case "$MODE" in
-    full) posting_full "$@";;
-    mono) posting_mono "$@";;
+    "full") posting_full "$@";;
+    "mono") posting_mono "$@";;
   esac
 }
 posting() {
-  posting_switch "$@"  >/dev/null
+  posting_switch "$@" >/dev/null
 }
 
 ### READING COMMAND ###
@@ -191,17 +230,13 @@ reading_mono() {
 }
 reading_switch() {
   case "$MODE" in
-    full) reading_full "$@";;
-    mono) reading_mono "$@";;
+    "full") reading_full "$@";;
+    "mono") reading_mono "$@";;
   esac
 }
 reading() {
   now
-  reading_switch "$@" \
-    | jq '. |= sort_by(.time) | reverse' \
-    | jq --raw-output '.[] | .user + " - " + .post + " (" + .time + ")"' \
-    | ago \
-    | cli
+  reading_switch "$@" | jq '. |= sort_by(.time) | reverse' | jq --raw-output '.[] | .user + " - " + .post + " (" + .time + ")"' | ago | cli
 }
 
 ### FOLLOWING COMMAND ###
@@ -214,9 +249,10 @@ following_full() {
   # using backend
   curl -s --location --request PUT "${API_BASE_URL}following?user=$1" \
     --header 'Content-Type: text/plain' \
-    --data-raw "$2"
+    --data-raw "$3"
 }
 following_mono() {
+  # without backend
   pushd "$MONO_PATH" > /dev/null
     # if missing, write <user name>.follows file
     # user wall contains zir own posts, thus add user to file
@@ -224,17 +260,20 @@ following_mono() {
     [[ -f "$follows" ]] || echo "$1" > "$follows"
     # if missing, append followed user to <user name>.follows file
     # user names not checked: assuming sunny day scenario :)
-    grep -qxF "$2" "$follows" || echo "$2" >> "$follows"
+    grep -qxF "$3" "$follows" || echo "$3" >> "$follows"
   popd > /dev/null
 }
 following_switch() {
   case "$MODE" in
-    full) following_full "$@";;
-    mono) following_mono "$@";;
+    "full") following_full "$@";;
+    "mono") following_mono "$@";;
   esac
 }
 following() {
-  following_switch "$@" >/dev/null
+  case $4 in
+    "") following_switch "$@" >/dev/null;;
+    *)  unrecognized "$4";;
+  esac
 }
 
 ### WALL COMMAND ###
@@ -248,6 +287,7 @@ wall_full() {
   curl -s --location --request GET "${API_BASE_URL}wall?user=$1"
 }
 wall_mono() {
+  # without backend
   pushd "$MONO_PATH" > /dev/null
     # loop <user name>.follows lines: they are followed users
     local follows="$1.follows"
@@ -259,51 +299,81 @@ wall_mono() {
 }
 wall_switch() {
   case "$MODE" in
-    full) wall_full "$@";;
-    mono) wall_mono "$@";;
+    "full") wall_full "$@";;
+    "mono") wall_mono "$@";;
   esac
 }
 wall() {
-  now
-  wall_switch "$@" \
-    | jq '. |= sort_by(.time) | reverse' \
-    | jq --raw-output '.[] | .user + " - " + .post + " (" + .time + ")"' \
-    | ago \
-    | cli
+  case $4 in
+    "") now
+        wall_switch "$@" | jq '. |= sort_by(.time) | reverse' | jq --raw-output '.[] | .user + " - " + .post + " (" + .time + ")"' | ago | cli;;
+    * ) unrecognized "$4";;
+  esac
 }
 
-### MAIN COMMAND STARTING WITH USER NAME ###
+################
+### COMMANDS ###
+################
+
+### <USER NAME> COMMAND ###
 username () {
   # return if empty line
   [ -z "$1" ] && return 0
   # check command after user name
   # each command calls a specific function
   case $2 in
-    "->")
-      posting "$@"
-      ;;
-    "")
-      reading "$1"
-      ;;
-    "follows")
-      following "$1" "$3"
-      ;;
-    "wall")
-      wall "$1"
-      ;;
-    *)
-      unrecognized "$2"
-      ;;
+    "->"      ) posting "$@";;
+    ""        ) reading "$@";;
+    "follows" ) following "$@";;
+    "wall"    ) wall "$@";;
+    *         ) unrecognized "$2";;
   esac
 }
 
-### MAIN CLI FUNCTION ###
-kata () {
+### HELP COMMAND ###
+help() {
+  cat <<-EOF
+
+kata commands:
+  <user name> -> <message>           -> post message to user timeline
+  <user name>                        -> read messages from user timeline
+  <user name> follows <another user> -> subscribe user to another user timeline
+  <user name> wall                   -> read messages from user timeline and subscriptions
+
+utility commands:
+  exit -> exit the cli
+  help -> read this help
+  kata -> read full readme of kata requirements
+  mode -> tell if running in:
+          - 'full' mode: attached to an API server which handles user data
+          - 'mono' mode: handling user data using the file system
+
+kata readme: https://github.com/xpeppers/social_networking_kata
+
+EOF
+}
+
+### KATA COMMAND ###
+kata() {
+  curl -s "https://raw.githubusercontent.com/xpeppers/social_networking_kata/master/README.md" | pandoc | lynx -stdin
+}
+
+### MODE COMMAND ###
+mode() {
+  echo "$PREFIX$MODE"
+}
+
+############
+### MAIN ###
+############
+
+### MAIN CLI LOOP ###
+main () {
+  # make standalone directory if needed
+  [ "$MODE" = mono ] && mkdir -p "$MONO_PATH"
   case $1 in
-    # normal execution
+    # normal interactive session
     "")
-      # make standalone directory if needed
-        [ "$MODE" = mono ] && mkdir -p .mono
       # loop until 'exit' command
       while read -r -e -p "$PREFIX" command
       do
@@ -311,36 +381,25 @@ kata () {
         history -s "$command"
         # check command
         case "$command" in
-          # exit command
-          "exit")
-            exit 0
-            ;;
-          # help command
-          "help")
-            help
-            ;;
-          # kata command to display github readme
-          "kata")
-            readme
-            ;;
-          # command starting with user name
-          *)
-            username $command
-            ;;
+          "exit" ) exit 0;;
+          "help" ) help;;
+          "kata" ) kata;; # display github readme
+          "mode" ) mode;; # display github readme
+          *      ) username $command;;
         esac
       done
       ;;
-    # run demo
-    demo)
+    # demo non-interactive session
+    "demo")
       # it's just a demo, bro
       demo=true
       # loop demo files
       set +o noglob
       for file in demo/*.demo
       do
-      set -o noglob
-      # count commands
-      commands=$(wc -l < "$file")
+        set -o noglob
+        # count commands
+        commands=$(wc -l < "$file")
         # loop demo file lines: they are the commands
         while read -r line
         do
@@ -350,7 +409,7 @@ kata () {
         done < "$file"
       done
       # let user read a bit more
-      sleep 3
+      sleep 2
       ;;
     # unrecognized command
     *)
@@ -362,34 +421,11 @@ kata () {
 ################################
 ### ENTRY POINT IS DOWN HERE ###
 ################################
-# check variables
-if [ -z "$MODE" ]
-then
-  echo "${PREFIX}missing environment variable: MODE" >&2
-  exit 1
-fi
-case "$MODE" in
-  "full")
-    if [ -z "$API_BASE_URL" ]
-    then
-      echo "${PREFIX}missing environment variable: API_BASE_URL" >&2
-      exit 1
-    fi
-  ;;
-  "mono")
-    if [ -z "$MONO_PATH" ]
-    then
-      echo "${PREFIX}missing environment variable: MONO_PATH" >&2
-      exit 1
-    fi
-    ;;
-  *)
-    echo "${PREFIX}unrecognized mode: $MODE" >&2
-    exit 1;;
-esac
+# check requirements
+reqcheck
 # move to script directory
 pushd "$(dirname "$0")" > /dev/null
-  # execute main cli function
-  kata "$@"
+  # run main cli loop
+  main "$@"
 # restore original directory
 popd > /dev/null
